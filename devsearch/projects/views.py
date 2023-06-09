9be84fbe6acd59abd17ca2dee2824ada024cf47e
over9003 <ing.gabriel.vartolomeu@gmail.com>
2023-06-09 23:30:58 +0300

    lesson 2 complete. templates and template inheritance

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def projects(request):
-    return HttpResponse("here are our projects")
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse(f"SIngle Pojrect: {pk} ")
+    return render(request, 'projects/single-project.html')
