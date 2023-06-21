3a9305da3a4e8377213fa418bc0d58423c955895
Gabriel Vartolomeu <gabriel.vartolomeu@keysight>
2023-06-21 18:02:13 +0300

    middle of lesson 6.3 8:23

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
+from django.db.models.signals import post_save
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -22,7 +24,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
@@ -33,3 +35,11 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+
+def profileUpdated(sender, instance, created, **kwargs):
+    print("profile saved")
+    print(f"Instance: {instance}")
+    print(f"Created: {created}")
+
+
+post_save.connect(profileUpdated, sender=Profile)
