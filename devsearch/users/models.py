219a85894eeed2bd71187b81b300abe1b4ee5404
Gabriel Vartolomeu <gabriel.vartolomeu@keysight>
2023-07-04 17:21:56 +0300

    changed penis to benis

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
     def __str__(self):
-        return str(self.username)
+        return f"{str(self.username)}"
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
